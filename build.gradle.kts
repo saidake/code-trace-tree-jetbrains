d6825157ed930f6fee0bfe91d035dc765d4dd569
Craig Brown <craigbrowndev@gmail.com>
2025-11-18 08:07:34 +0800

    [Global] ci: Remove format check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.24" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 tasks.named<Wrapper>("wrapper") {
@@ -10,19 +9,5 @@ tasks.named<Wrapper>("wrapper") {
 allprojects {
     repositories {
         mavenCentral()
-    }
-}
-// Apply ktlint to root + all subprojects
-apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-// Global ktlint configuration (applies to root and all subprojects)
-ktlint {
-    verbose.set(true)
-    outputToConsole.set(true)
-    coloredOutput.set(true)
-    ignoreFailures.set(false) // fails the build on violations
-    // ktlint already includes *.kt and *.kts files anywhere in src/main/kotlin, src/test/kotlin, etc.
-    filter {
-        exclude("**/generated/**")
     }
 }
